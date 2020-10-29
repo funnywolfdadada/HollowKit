0e19db52e3ab8ed32f213acf9f13def805d94d59
funnywolf <1748787587@qq.com>
2020-10-29 23:35:39 +0800

    圆角矩形，支持边框和 [Shader]

--- app/src/main/java/com/funnywolf/hollowkit/TestScene.kt
+++ app/src/main/java/com/funnywolf/hollowkit/TestScene.kt
@@ -1,12 +1,15 @@
 package com.funnywolf.hollowkit
 
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.LinearLayout
+import android.widget.ScrollView
 import com.bytedance.scene.Scene
+import com.funnywolf.hollowkit.drawable.LinearGradientProvider
+import com.funnywolf.hollowkit.drawable.RoundRectDrawable
 import com.funnywolf.hollowkit.utils.*
 
 /**
@@ -21,10 +24,68 @@ class TestScene: Scene() {
         savedInstanceState: Bundle?
     ): View {
         val context = inflater.context
-        val rv = RecyclerView(context).apply {
-
+        return ScrollView(context).apply {
+            val linearLayout = LinearLayout(context).apply {
+                setBackgroundColor(0xFF000000.toInt())
+                orientation = LinearLayout.VERTICAL
+                addView(View(context).apply {
+                    background = RoundRectDrawable(0x80FFFFFF.toInt(), 50.dp, inverse = false).apply {
+                        ringColor = 0xFFFFFFFF.toInt()
+                        ringSize = 40.dp
+                    }
+                }, ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 200.dp))
+                addView(View(context).apply {
+                    background = RoundRectDrawable(0xFF00FFFF.toInt(), 50.dp, inverse = true).apply {
+                        ringColor = 0xFFFFFFFF.toInt()
+                        ringSize = 10.dp
+                    }
+                }, ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 200.dp))
+                addView(View(context).apply {
+                    background = RoundRectDrawable(0xFFFFFFFF.toInt(), 100.dp, inverse = false).apply {
+                        fillShaderProvider = LinearGradientProvider(GradientDrawable.Orientation.RIGHT_LEFT, intArrayOf(0xFFFF0000.toInt(), 0xFF00FF00.toInt(), 0xFF0000FF.toInt()))
+                        ringColor = 0xFFFFFFFF.toInt()
+                        ringSize = 50.dp
+                        ringShaderProvider = LinearGradientProvider(GradientDrawable.Orientation.BOTTOM_TOP, intArrayOf(0xFFFF0000.toInt(), 0xFF00FF00.toInt(), 0xFF0000FF.toInt()))
+                    }
+                }, ViewGroup.LayoutParams(200.dp, 200.dp))
+                addView(View(context).apply {
+                    background = RoundRectDrawable(0xFFFFFFFF.toInt(), 100.dp, inverse = true).apply {
+                        ringColor = 0xFFFFFFFF.toInt()
+                        ringSize = 0.dp
+                        fillShaderProvider = LinearGradientProvider(GradientDrawable.Orientation.BR_TL, intArrayOf(0xFFFF0000.toInt(), 0xFF00FF00.toInt(), 0xFF0000FF.toInt()))
+                    }
+                }, ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 200.dp))
+                addView(View(context).apply {
+                    background = RoundRectDrawable(0xFFFFFFFF.toInt(), 20.dp, inverse = false).apply {
+                        ringColor = 0xFFFFFFFF.toInt()
+                        ringSize = 10.dp
+                        fillShaderProvider = LinearGradientProvider(GradientDrawable.Orientation.BOTTOM_TOP, intArrayOf(0xFFFF0000.toInt(), 0xFF00FF00.toInt(), 0xFF0000FF.toInt()))
+                    }
+                }, ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 200.dp))
+                addView(View(context).apply {
+                    background = RoundRectDrawable(0xFFFFFFFF.toInt(), 20.dp, inverse = false).apply {
+                        ringColor = 0xFFFFFFFF.toInt()
+                        ringSize = 10.dp
+                        fillShaderProvider = LinearGradientProvider(GradientDrawable.Orientation.BL_TR, intArrayOf(0xFFFF0000.toInt(), 0xFF00FF00.toInt(), 0xFF0000FF.toInt()))
+                    }
+                }, ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 200.dp))
+                addView(View(context).apply {
+                    background = RoundRectDrawable(0xFFFFFFFF.toInt(), 20.dp, inverse = false).apply {
+                        ringColor = 0xFFFFFFFF.toInt()
+                        ringSize = 10.dp
+                        fillShaderProvider = LinearGradientProvider(GradientDrawable.Orientation.LEFT_RIGHT, intArrayOf(0xFFFF0000.toInt(), 0xFF00FF00.toInt(), 0xFF0000FF.toInt()))
+                    }
+                }, ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 200.dp))
+                addView(View(context).apply {
+                    background = RoundRectDrawable(0xFFFFFFFF.toInt(), 20.dp, inverse = false).apply {
+                        ringColor = 0xFFFFFFFF.toInt()
+                        ringSize = 10.dp
+                        fillShaderProvider = LinearGradientProvider(GradientDrawable.Orientation.TL_BR, intArrayOf(0xFFFF0000.toInt(), 0xFF00FF00.toInt(), 0xFF0000FF.toInt()))
+                    }
+                }, ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 200.dp))
+            }
+            addView(linearLayout)
         }
-        return rv
     }
 
 }
--- hollowkit/src/main/java/com/funnywolf/hollowkit/drawable/RoundRectDrawable.kt
+++ hollowkit/src/main/java/com/funnywolf/hollowkit/drawable/RoundRectDrawable.kt
@@ -2,15 +2,17 @@ package com.funnywolf.hollowkit.drawable
 
 import android.graphics.*
 import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
+import kotlin.math.hypot
 
 
 /**
- * 圆角矩形
+ * 圆角矩形，支持边框和 [Shader]
  *
  * @author https://github.com/funnywolfdadada
  * @since 2020/7/3
  */
-class RoundRectDrawable(
+open class RoundRectDrawable(
     color: Int,
     leftTop: Int,
     rightTop: Int = leftTop,
@@ -22,10 +24,42 @@ class RoundRectDrawable(
     private val inverse: Boolean = false
 ): Drawable() {
 
+    var fillColor = color
+        set(value) {
+            field = value
+            invalidateSelf()
+        }
+
+    var fillShaderProvider: ShaderProvider? = null
+        set(value) {
+            field = value
+            updateShader()
+            invalidateSelf()
+        }
+
+    var ringColor: Int = 0
+        set(value) {
+            field = value
+            invalidateSelf()
+        }
+
+    var ringShaderProvider: ShaderProvider? = null
+        set(value) {
+            field = value
+            updateShader()
+            invalidateSelf()
+        }
+
+    var ringSize: Int = 0
+        set(value) {
+            field = value
+            updateRingPath()
+            invalidateSelf()
+        }
+
     private val paint = Paint().also {
         it.isAntiAlias = true
         it.style = Paint.Style.FILL
-        it.color = color
     }
 
     private val radii = floatArrayOf(
@@ -34,25 +68,58 @@ class RoundRectDrawable(
         rightBottom.toFloat(), rightBottom.toFloat(),
         leftBottom.toFloat(), leftBottom.toFloat()
     )
+    private val fillPath = Path()
+    private var fillShader: Shader? = null
 
-    private val path = Path()
+    private val innerRadii = FloatArray(radii.size)
+    private val ringPath = Path()
+    private var ringShader: Shader? = null
 
     override fun onBoundsChange(bounds: Rect?) {
         super.onBoundsChange(bounds)
-        bounds ?: return
-        path.apply {
-            rewind()
-            fillType = if (inverse) {
-                Path.FillType.INVERSE_WINDING
-            } else {
-                Path.FillType.WINDING
-            }
-            addRoundRect(RectF(bounds), radii, Path.Direction.CW)
+        updateFillPath()
+        updateRingPath()
+        updateShader()
+    }
+
+    private fun updateFillPath() {
+        fillPath.rewind()
+        fillPath.fillType = if (inverse) {
+            Path.FillType.INVERSE_WINDING
+        } else {
+            Path.FillType.WINDING
         }
+        fillPath.addRoundRect(RectF(bounds), radii, Path.Direction.CW)
+    }
+
+    private fun updateRingPath() {
+        if (ringSize <= 0) {
+            return
+        }
+        val bf = RectF(bounds)
+        val ratio = 1 - 2 * ringSize / hypot(bf.width(), bf.height())
+        for (i in innerRadii.indices) {
+            innerRadii[i] = radii[i] * ratio
+        }
+        ringPath.rewind()
+        ringPath.addRoundRect(bf.left, bf.top, bf.right, bf.bottom, radii, Path.Direction.CW)
+        ringPath.addRoundRect(bf.left + ringSize, bf.top + ringSize, bf.right - ringSize, bf.bottom - ringSize, innerRadii, Path.Direction.CCW)
+    }
+
+    private fun updateShader() {
+        fillShader = fillShaderProvider?.invoke(bounds)
+        ringShader = ringShaderProvider?.invoke(bounds)
     }
 
     override fun draw(canvas: Canvas) {
-        canvas.drawPath(path, paint)
+        paint.shader = fillShader
+        paint.color = fillColor
+        canvas.drawPath(fillPath, paint)
+        if (ringColor != 0 && ringSize != 0) {
+            paint.shader = ringShader
+            paint.color = ringColor
+            canvas.drawPath(ringPath, paint)
+        }
     }
 
     override fun setAlpha(alpha: Int) {
@@ -68,3 +135,45 @@ class RoundRectDrawable(
     }
 
 }
+
+interface ShaderProvider: Function1<Rect, Shader>
+
+class LinearGradientProvider(
+    private val orientation: GradientDrawable.Orientation,
+    private val colors: IntArray,
+    private val positions: FloatArray? = null,
+    private val model: Shader.TileMode = Shader.TileMode.CLAMP
+): ShaderProvider {
+
+    override fun invoke(r: Rect): Shader {
+        val p = when (orientation) {
+            GradientDrawable.Orientation.TOP_BOTTOM -> {
+                RectF(0F, r.top.toFloat(), 0F, r.bottom.toFloat())
+            }
+            GradientDrawable.Orientation.TR_BL -> {
+                RectF(r.right.toFloat(), r.top.toFloat(), r.left.toFloat(), r.bottom.toFloat())
+            }
+            GradientDrawable.Orientation.RIGHT_LEFT -> {
+                RectF(r.right.toFloat(), 0F, r.left.toFloat(), 0F)
+            }
+            GradientDrawable.Orientation.BR_TL -> {
+                RectF(r.right.toFloat(), r.bottom.toFloat(), r.left.toFloat(), r.top.toFloat())
+            }
+            GradientDrawable.Orientation.BOTTOM_TOP -> {
+                RectF(0F, r.bottom.toFloat(), 0F, r.top.toFloat())
+            }
+            GradientDrawable.Orientation.BL_TR -> {
+                RectF(r.left.toFloat(), r.bottom.toFloat(), r.right.toFloat(), r.top.toFloat())
+            }
+            GradientDrawable.Orientation.LEFT_RIGHT -> {
+                RectF(r.left.toFloat(), 0F, r.right.toFloat(), 0F)
+            }
+            GradientDrawable.Orientation.TL_BR -> {
+                RectF(r.left.toFloat(), r.top.toFloat(), r.right.toFloat(), r.bottom.toFloat())
+            }
+        }
+        return LinearGradient(p.left, p.top, p.right, p.bottom, colors, positions, model)
+    }
+
+}
+
